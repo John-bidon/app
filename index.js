3f8e7eea9e424d39a9bde25a6c20b7cfb53d2dcb
pavlo.veselov <7262830@gmail.com>
2023-09-28 19:23:09 +0300

    index main run

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-//test
 function main(args) {
   let name = args.name || 'stranger'
   let greeting = 'Hello ' + name + '!'
   console.log(greeting)
   return {"body": greeting}
 }
+
+main('John Bidon');
